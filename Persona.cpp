7b7cc47908d5712152c4df622305261492616dfb
Andrianochka <Andriana-zima@yandex.ru>
2014-03-19 09:11:27 +0200

    Update Persona.cpp

--- Persona.cpp
+++ Persona.cpp
@@ -1,19 +1,19 @@
-#include <iostream.h>
+#include <iostream>
 #include <string.h>
 using namespace std;
 
 
-
 class Persona
-{
-char name;              // имя
-int age;                  // возраст
-int state;                 // пол
+{ protected:
+            
+   char* name;              // имя
+   int age;                  // возраст
+   int state;                 // пол
 
 public:     
-Persona();                               // конструктор без параметров
-Persona(char*,int,int);         // конструктор с параметрами
-Persona(const Persona&); // конструктор копирования
+Persona();                               //конструктор без параметров
+Persona(char* name,int age,int state);   // конструктор з параметрами
+Persona(const Persona&);                // конструктор копыювання
 
 ~Persona();
 char * GetName() ;
@@ -25,49 +25,36 @@ void SetAge(int);
 void SetState(int);
 void Set(char*,int,int);
 void Show(); 
-}
+};
 Persona::Persona()
 { 
-
 name = new char[20];
 name[0]=0;
 age=0;
 state=0;
-cout<<"constructor bez parametriv"<< endl;
-}
+};
   
- void persona::show();
-{
-cout<<"("«name«","«age«","«state«")"
-}
-Persona::Persona(char*_name, int_sge, int_state);
+
+Persona::Persona(char* _name, int _age, int _state)
  { 
- int n=strlen(new_name);
+  int n=strlen(_name);
  this->name=new char[n+1];
- strlen cpy(name, new_name,n);
-  this->age=new_age[n+1];
- strlen cpy(age, new_age,n);
- this->state=new_state[n+1];
- strlen cpy(state, new_state,n);
+ strncpy(name, _name,n);
+ this->age=_age;
+ this->state=_state;
  }  
-   void Persona::show_Persona(void)
+   void Persona::Show()
 { 
- cout « "name: " « name « endl;
- cin » name;
- cout « "age: " « age « endl; 
- cin » age;
- cout « "state: " « state « endl; 
- cin » state; 
+ cout << "name: " << name <<"age: " << age << "state: " << state << endl; 
+
 }
-cout <<"konstrucror z parametrami"<<endl;
- }
   
 int main(int argc, char *argv[])
 { Persona p1;
   p1.show();
-  Persona p2;
+  Persona p2(olya,4,1);
   p2.show();
   void Persona();
     system("PAUSE");
-    return p1;
+    return p2;
 }
